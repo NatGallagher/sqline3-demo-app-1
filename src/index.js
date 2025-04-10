ac15d20999cec647160b25e8183590dbab3d7ef4
NatGallagher <natgallagher@Nats-Air.attlocal.net>
2025-04-10 15:31:44 -0400

    update

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@
 
 const sqlite3 = require('sqlite3').verbose();
 
-//windwos
-//let _dbname = "data\\demodb1.db"
-let _dbname ="src/data/dashboard.db"
+//windwos or macos or linux 
+//let _dbname = `src\\data\\demodb1.db`
+//let _dbname = "src\\data\dashboard.db"
 
 //macos, linux os 
-//let _dbname = "data/demodb1.db"
+//let _dbname = "src/data/demodb1.db"
+let _dbname = "src/data/dashboard.db"
 
 let db = new sqlite3.Database(_dbname, sqlite3.OPEN_READWRITE, (err) => {
   if (err) {
@@ -16,9 +17,28 @@ let db = new sqlite3.Database(_dbname, sqlite3.OPEN_READWRITE, (err) => {
   console.log('Connected successfully.');
 });
 
-db.all("SELECT * FROM user", [], (err, row) => {
+//insert row 
+insert_query = "INSERT INTO user(username, password) VALUES(?, ?)"
+values = ["usernine", "pwd9"]
+//db.run(insert_query, values)
+
+console.log("row inserted");
+
+
+//returns all rows 
+db.all("SELECT * FROM user", [], (err, rows) => {
+    console.log(rows)
+})
+
+//returns first row
+//db.get("SELECT * FROM user", [], (err, row) => {
+
+
+
+db.get("SELECT * FROM user where username=? and password=?", ["user4","pwd4"], (err, row) => {
     if (err) {
-      throw err; 
+        throw err; 
     }
     console.log(row);
 });
+
